af6f274c7ee339abca28e895b3d5c600a4337c9c
Minura Silva <minura.silva@outlook.com>
2020-06-08 06:05:58 -0700

    add Elasticsearch indexing. Needs Elasticsearch to be running.

--- sports_scraper/settings.py
+++ sports_scraper/settings.py
@@ -62,11 +62,15 @@ ROBOTSTXT_OBEY = True
 #    'scrapy.extensions.telnet.TelnetConsole': None,
 #}
 
+# Elasticsearch settings
+ES_URI = 'localhost'
+ES_PORT = '9200'
+
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'sports_scraper.pipelines.SportsScraperPipeline': 300,
-#}
+ITEM_PIPELINES = {
+   'sports_scraper.pipelines.ElasticsearchPipeline': 100,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
